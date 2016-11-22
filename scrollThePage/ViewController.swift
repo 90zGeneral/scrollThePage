156c17f3ddd0da66cf2eab68a34b78d10de4ddd8
Roydon Jeffrey <brushdam14823@gmail.com>
2016-11-22 15:31:25 -0500

    Still trying to figure out how to implement gesture recognizer

--- scrollThePage/ViewController.swift
+++ scrollThePage/ViewController.swift
@@ -10,43 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Outlet
+    @IBOutlet var myView: UIImageView!
     @IBOutlet var scrollView: UIScrollView!
     
+    //Array to hold the images as imageViews
     var images = [UIImageView]()
+    
+    let panRec = UIPanGestureRecognizer()
+    let tapRec = UITapGestureRecognizer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        panRec.addTarget(self, action: #selector(ViewController.myViewTime))
+        tapRec.addTarget(self, action: #selector(ViewController.scrollViewTime))
+        
+        myView.addGestureRecognizer(panRec)
+        scrollView.addGestureRecognizer(tapRec)
+        
+        myView.isUserInteractionEnabled = true
+        scrollView.isUserInteractionEnabled = true
+        
+    }
+    
+    func myViewTime() {
+        
+    }
+    
+    func scrollViewTime() {
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         
+        //Creating containers to contain the width of the content and the scrollView
         var contentWidth: CGFloat = 0.0
-        
         let scrollWidth = scrollView.frame.size.width
         
+        //Loop for the images prefixed by "icon" and suffixed with numbers ranging from 0-2
         for x in 0...2 {
             
+            //Grab each image and convert them into UIImageViews and then append them to the images array
             let image = UIImage(named: "icon\(x)")
             let imageView = UIImageView(image: image)
             images.append(imageView)
             
+            //Determining the x coordinate for the imageView and determining the width of the content within the scrollView
             var newX: CGFloat = 0.0
-            
             newX = (scrollWidth / 2) + (scrollWidth * CGFloat(x))
+            contentWidth = newX + (scrollWidth / 2)
             
-            contentWidth = newX + scrollWidth / 2
-            
+            //Add the imageView to the scrollView
             scrollView.addSubview(imageView)
             
+            //Setting the position of the imageView in the center while determining the size by width & height
             imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
             
         }
-        
+        //Setting the size of the content within the scrollView
         scrollView.contentSize = CGSize(width: contentWidth, height: scrollView.frame.size.height)
-        scrollView.clipsToBounds = false
         
+        //Disconnect the next imageView from the scrollView bounds so it appears that it's coming in from the edge of the UIView
+        scrollView.clipsToBounds = false
         
     }
 
